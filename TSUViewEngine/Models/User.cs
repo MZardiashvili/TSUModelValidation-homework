520178c7bddf958987e0eef4db3ac28d6dbcfcfc
Mariami-137 <Mariami-137@github.com>
2020-05-14 01:14:20 +0400

    Add project files.

---
+++ TSUViewEngine/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TSUViewEngine.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Lastname { get; set; }
+
+        [Required(ErrorMessage = "ველი სავალდებულოა")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "ელ. ფოსტის მისამართი არ არის ვალიდური")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage ="პაროლი უნდა შეიცავდეს მინიმუმ {2} სიმბოლოს", MinimumLength=8)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "პაროლები არ ემთხვევა ერთმანეთს")]
+        public string PasswordConfirmation { get; set; }
+    }
+}
